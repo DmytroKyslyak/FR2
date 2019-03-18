c21f47396ea0b7f44d637ffdebdbe97a3c30d567
DmytroKyslyak <42807690+DmytroKyslyak@users.noreply.github.com>
2019-03-18 21:16:32 +0200

    cw10
    cw10

--- jq/scripts/menu.js
+++ jq/scripts/menu.js
@@ -4,12 +4,45 @@ class b_menu{
 		
 		this.createEvents();
 		}
+	
+	toggleSubmenu(jqMenuItem){
+		jqMenuItem.children('.b-submenu').stop().slideToggle('b-submenu_shown');
+	}
+	
 	showSubmenu(event){
 		console.log('showSubmenu event', event);
-		}
-		
+		let jqMenuItem = $(event.currentTarget);
+			//jqMenuItem.children('.b-submenu').show();
+			//jqMenuItem.children('.b-submenu').addClass('b-submenu_shown');
+			//jqMenuItem.children('.b-submenu').stop().slideDown('b-submenu_shown');
+			//jqMenuItem.children('.b-submenu').stop().slideToggle('b-submenu_shown'); //делает противоположное, тумблер
+			//this.toggleSubmenu(jqMenuItem);
+			jqMenuItem.children('.b-submenu')
+					.css({'display' : 'block'})
+					.stop()
+					.animate(
+						{'opacity' : 1} 
+						,400
+						);
+			}
+			
+	hideSubmenu(event){
+		console.log('hideSubmenu event', event);
+		let jqMenuItem = $(event.currentTarget);
+			//jqMenuItem.children('.b-submenu').hide();
+			//jqMenuItem.children('.b-submenu').removeClass('b-submenu_shown');
+			//jqMenuItem.children('.b-submenu').stop().slideUp('b-submenu_shown');
+			//jqMenuItem.children('.b-submenu').stop().slideToggle('b-submenu_shown');
+			//this.toggleSubmenu(jqMenuItem);
+			
+			}
+	
 	createEvents(){
 		// при наведении мыши на пункт меню - показать подменю
-		this.menu.find('.b-menu__item').mouseover(this.showSubmenu);
+		this.menu.find('.b-menu__item')
+			.mouseover(this.showSubmenu.bind(this)) //.bind(this)-чтобы указатели на методы-обработчики событий корректно работали с this при описании событий мы это и пишем.
+			.mouseout(this.hideSubmenu.bind(this))
+			;
+
 		}
 }
