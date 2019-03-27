825f095c66311c3e605dd6e23042b9b07781716c
DmytroKyslyak <42807690+DmytroKyslyak@users.noreply.github.com>
2019-03-27 21:26:13 +0200

    cw122
    cw122

--- jq/scripts/form1.js
+++ jq/scripts/form1.js
@@ -3,28 +3,85 @@ class b_form {
 		this.form = $(sSelector); //шаг4 ****$ авто+водитель+gps **** sSelector-место назначения(адрес)
 		this.createEvents();
 	}
+	getLanguages(){
+				let 
+			oLanguages = {	
+				 "ru"	: "Русский"
+				,"en"	: "Английский"
+				,"uk"	: "Украинский"
+				,"de"	: "Немецкий"
+				}
+			,sLanguages = ''
+			;
+		$.each(oLanguages,(sLanguageCode,sLanguageName)=>{	//$. возвращение jq обект
+			let jqLanguages = $(oLanguages);
+				sLanguages += 		
+					(sLanguages		
+						? '\n'		
+						:''			
+						) 			
+					+ sLanguageCode + ' - ' + sLanguageName; 
+		});		
+
+//sLanguages += (sLanguages ? "/n" : '') + sLanguageCode + ' - ' + sLanguageName;		
+		return sLanguages;
+	}
+	
+	
+	label(sLabelSelector){
+		return this.form.find(sLabelSelector).html();
+	}
+	
+	val(sFieldSelector){
+		let
+			jqField = this.form.find(sFieldSelector)
+			,sFieldType = jqField.attr('type')
+			;
+		//2. тег(select)
+		//3. флажки (type='checkbox')
+		
+		if(sFieldType=='radio'){					//1. переключатели (type='radio')
+			return jqField.filter(':checked').val();
+		}
+		else if(0){}
+		else if(0){}
+		else {					//4. текстовое поле
+			return jqField.val();	
+			}
+		
+	}
 	
 	showInfo(event){ // метод
 		// currentTarget - ссылка на него, с чем происходит работа
-		let jpFormOKButton = $(event.currentTarget);
+		let jpFormOKButton = $(event.currentTarget)
+		,sSeasons = ''
+		;
 		
-		this.form.find('.b-form__season')	// обект 
+		this.form.find('.b-form__season:checked')	// обект 
 			.each(							// цикл
-			(i, oSeason)=>{					// функция которая содержит команды тела цикла
-				let jqSeason = $(oSeason);
+			(i, /*JS объект*/oSeason)=>{					// функция которая содержит команды тела цикла
+				let jqSeason = $(oSeason); // $-функция выполняет глобальный поиск в пределах всего html документа по css селектору (или js обекту) и возвращаем - jq объект который является коллекцией. Коллекция - массив.
+				sSeasons += 		// в переменную допишем время года
+					(sSeasons		// если есть sSeasons
+						?', '		// то значение "запятая"
+						:''			// иначе значение "ничего"
+						) 			// тернарный опреатор
+					+ jqSeason.val(); 	// и склеивает JQ-обекта флажка сезон
 				}
 			);
-		
-		alert(
-			'Ваша форма:'
-			+ '\nИмя: '						+ this.form.find('.b-form__name').val()
-			+ '\nФамилия: '					+ this.form.find('.b-form__surname').val()
-			+ '\nВаше любимое время года: '	+ this.form.find('.b-form__season').val()
-			+ '\nВаш любимый фрукт: '		+ this.form.find('.b-form__fruit:checked').val()
-			+ '\nВаше любимое животное: '	+ this.form.find('.b-form__animals>option:selected').html()
-			+ '\nВаш отзыв: '				+ this.form.find('.b-form__review').val()
-			);
-		} 
+// ` мультистроки
+// ${} строка подстановки		
+	alert(
+		'Ваша форма:'									//	+ this.form.find('.b-form__name')
+		+ `\n${this.label('.b-form__label_name')}: `	+ this.val('.b-form__name')
+		+ `\n${this.label('.b-form__label_surname')}: `	+ this.val('.b-form__surname')
+		+ `\n${this.label('.b-form__label_season')}: `	+ sSeasons
+		+ `\n${this.label('.b-form__label_fruit')}: `	+ this.val('.b-form__fruit:checked')
+		+ `\n${this.label('.b-form__label_animals')}: `	+ this.val('.b-form__animals>option:selected')//.html()
+		+ `\n${this.label('.b-form__label_review')}: `	+ this.val('.b-form__review')
+		+ '\nЯзыки:\n'									+ this.getLanguages()
+		);
+	} 
 	
 	createEvents(){// шаг5
 		this.form.find('.b-form__ok-button').click(this.showInfo.bind(this));
