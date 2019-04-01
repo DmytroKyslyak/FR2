37015ccc9749e9d947ee78a18f0747fbf2cd0629
DmytroKyslyak <42807690+DmytroKyslyak@users.noreply.github.com>
2019-04-01 21:18:15 +0300

    cw14
    cw14

--- jq/scripts/form1.js
+++ jq/scripts/form1.js
@@ -36,48 +36,51 @@ class b_form {
 		let
 			jqField = this.form.find(sFieldSelector)
 			,sFieldType = jqField.attr('type')
+			,sFieldTagName =  jqField.prop('tagName')
 			;
-		//2. тег(select)
-		//3. флажки (type='checkbox')
 		
 		if(sFieldType=='radio'){					//1. переключатели (type='radio')
 			return jqField.filter(':checked').val();
 		}
-		else if(0){}
-		else if(0){}
+		else if(sFieldTagName == 'SELECT'){			//2. тег(SELECT)
+			//	this.form.find('('.b-form__animals>option:selected').html()')
+		return jqField.children('option').filter(':selected').html();
+		}
+		else if(sFieldType=='checkbox'){		//3. флажки (type='checkbox')
+				let sCheckboxes = '';
+			jqField.filter(':checked')			// обект 
+				.each(							// цикл
+				(i, /*JS объект*/oCheckbox)=>{	// функция которая содержит команды тела цикла
+					let jqCheckbox = $(oCheckbox); 	// $-функция выполняет глобальный поиск в пределах всего html документа по css селектору (или js обекту) и возвращаем - jq объект который является коллекцией. Коллекция - массив.
+					sCheckboxes += 		// в переменную допишем время года
+						(sCheckboxes		// если есть sCheckboxes
+							?', '		// то значение "запятая"
+							:''			// иначе значение "ничего"
+							) 			// тернарный опреатор
+						+ jqCheckbox.val(); 	// и склеивает JQ-обекта флажка сезон
+					}
+				);
+		
+			return sCheckboxes;
+		}   
 		else {					//4. текстовое поле
 			return jqField.val();	
 			}
-		
 	}
 	
 	showInfo(event){ // метод
 		// currentTarget - ссылка на него, с чем происходит работа
-		let jpFormOKButton = $(event.currentTarget)
-		,sSeasons = ''
-		;
+		let jpFormOKButton = $(event.currentTarget);
 		
-		this.form.find('.b-form__season:checked')	// обект 
-			.each(							// цикл
-			(i, /*JS объект*/oSeason)=>{					// функция которая содержит команды тела цикла
-				let jqSeason = $(oSeason); // $-функция выполняет глобальный поиск в пределах всего html документа по css селектору (или js обекту) и возвращаем - jq объект который является коллекцией. Коллекция - массив.
-				sSeasons += 		// в переменную допишем время года
-					(sSeasons		// если есть sSeasons
-						?', '		// то значение "запятая"
-						:''			// иначе значение "ничего"
-						) 			// тернарный опреатор
-					+ jqSeason.val(); 	// и склеивает JQ-обекта флажка сезон
-				}
-			);
 // ` мультистроки
 // ${} строка подстановки		
 	alert(
-		'Ваша форма:'									//	+ this.form.find('.b-form__name')
+		`${settings.get('b-form__yourform')}:`									//	+ this.form.find('.b-form__name')
 		+ `\n${this.label('.b-form__label_name')}: `	+ this.val('.b-form__name')
 		+ `\n${this.label('.b-form__label_surname')}: `	+ this.val('.b-form__surname')
-		+ `\n${this.label('.b-form__label_season')}: `	+ sSeasons
-		+ `\n${this.label('.b-form__label_fruit')}: `	+ this.val('.b-form__fruit:checked')
-		+ `\n${this.label('.b-form__label_animals')}: `	+ this.val('.b-form__animals>option:selected')//.html()
+		+ `\n${this.label('.b-form__label_season')}: `	+ this.val('.b-form__season')
+		+ `\n${this.label('.b-form__label_fruit')}: `	+ this.val('.b-form__fruit')
+		+ `\n${this.label('.b-form__label_animals')}: `	+ this.val('.b-form__animals')//.html()
 		+ `\n${this.label('.b-form__label_review')}: `	+ this.val('.b-form__review')
 		+ '\nЯзыки:\n'									+ this.getLanguages()
 		);
---
+++ jq/scripts/init_settings.js
@@ -0,0 +1,12 @@
+let _page_settings = {
+	 'b-form__yourform' 	: 'Ваша форма'
+	,'b-form__languages' 	: 'Языки'
+	,'langs' : {	
+			 "ru"	: "Русский"
+			,"en"	: "Английский"
+			,"uk"	: "Украинский"
+			,"de"	: "Немецкий"
+			}
+	};
+
+settings.init(_page_settings);
