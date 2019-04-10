057adb819553ed594deb1981763dfbd4abce7c18
DmytroKyslyak <42807690+DmytroKyslyak@users.noreply.github.com>
2019-04-10 21:15:45 +0300

    cw17
    cw17

--- jq/scripts/gallery.js
+++ jq/scripts/gallery.js
@@ -8,19 +8,38 @@ class b_gallery /* extends Component */ {
 		this.arrowNext 		= this.gallery.find('.b-preview__arrow_next');
 		this.previewImage 	= this.gallery.find('.b-preview__image');
 		this.previewText 	= this.gallery.find('.b-preview__text');
+		this.currentImageNumber = 0;
 		
 		this.createEvents();
 		}
 
 	showPreview(event){ 
-		let jqPicture = $(event.currentTarget)
-		,jqSmallImage = jqPicture.find('.b-picture__image')
-		//......
-		;
+		let 
+		jqPicture = $(event.currentTarget); 	//jqPicture - small везде
+			
+		this.currentImageNumber = this.pictures.index(jqPicture);	//найти индекс картинки и присвоить
+		
+		this.showImage(0);		// ок, show image with step 0 (no step)
+		
+		this.preview.addClass('b-preview_shown');
 		}
-    hidePreview (event){ }
-	showPrevious (event){ }
-    showNext (event){ }
+		
+    hidePreview 	(event){ }
+	
+	showImage(iStep){
+		this.currentImageNumber +=iStep;
+		
+		let jqPicture = this.pictures.eq(this.currentImageNumber)	//jqPicture - small везде
+			,jqSmallImage = jqPicture.find('.b-picture__image')
+			,sSmallImageSrc = jqSmallImage.attr('src')	//attr тут отвечает за считывание
+			,sBigImageSrc = sSmallImageSrc.replace('small_','')
+			;
+			
+		this.previewImage.attr('src',sBigImageSrc); //attr тут отвечает за запись, есть 2-й параметр
+	}
+	
+	showPrevious 	(event){this.showImage(-1); }
+    showNext 		(event){this.showImage(+1); }
 	
 	
 	createEvents(){
